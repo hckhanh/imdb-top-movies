2d99b8d21cb354db20cb362f7a3545ae1c4d6060
hckhanh <hoangchaukhanh93@gmail.com>
2015-08-13 22:07:51 +0700

    Remove unused code and test Memory Leak
    Memory Leak: OK!

--- client/IMDBTopMovies2/app/src/main/java/com/demo/imdbtopmovies/data/Movie.java
+++ client/IMDBTopMovies2/app/src/main/java/com/demo/imdbtopmovies/data/Movie.java
@@ -1,12 +1,9 @@
 package com.demo.imdbtopmovies.data;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 /**
  * Created by hckhanh on 01/08/2015.
  */
-public class Movie implements Parcelable {
+public class Movie {
 
     private int rank;
 
@@ -25,44 +22,4 @@ public class Movie implements Parcelable {
         return name;
     }
 
-    /**
-     * Describe the kinds of special objects contained in this Parcelable's
-     * marshalled representation.
-     *
-     * @return a bitmask indicating the set of special object types marshalled
-     * by the Parcelable.
-     */
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    /**
-     * Flatten this object in to a Parcel.
-     *
-     * @param out  The Parcel in which the object should be written.
-     * @param flags Additional flags about how the object should be written.
-     *              May be 0 or {@link #PARCELABLE_WRITE_RETURN_VALUE}.
-     */
-    @Override
-    public void writeToParcel(Parcel out, int flags) {
-        out.writeInt(rank);
-        out.writeString(name);
-    }
-
-    public static final Parcelable.Creator<Movie> CREATOR
-            = new Parcelable.Creator<Movie>() {
-        public Movie createFromParcel(Parcel in) {
-            return new Movie(in);
-        }
-
-        public Movie[] newArray(int size) {
-            return new Movie[size];
-        }
-    };
-
-    private Movie(Parcel in) {
-        rank = in.readInt();
-        name = in.readString();
-    }
 }
