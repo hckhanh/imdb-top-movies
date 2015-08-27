b8a5461b64dc11c70474c2e7811bf07cdedb0ec4
hckhanh <hoangchaukhanh93@gmail.com>
2015-08-28 01:04:48 +0700

    Fix run-emulator.sh path

--- travis-script/run-client-test.sh
+++ travis-script/run-client-test.sh
@@ -13,7 +13,7 @@ if [ -d "$ANDROID_DIR/$ANDROID_PROJECT_NAME" ]; then
 	./gradlew build
 
 	# Run android emulator
-	./$TRAVIS_BUILD_DIR/travis-script/run-emulator.sh
+	$TRAVIS_BUILD_DIR/travis-script/run-emulator.sh
 
 	# Run connectedCheck (cC) of Android Apps
 	./gradlew connectedCheck
