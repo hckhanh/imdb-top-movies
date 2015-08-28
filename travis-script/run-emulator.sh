ce200a604cd2a141e603d1650e66a62893590a2c
hckhanh <hoangchaukhanh93@gmail.com>
2015-08-28 09:09:41 +0700

    Edit some script files
    Complete test configuration

--- travis-script/run-emulator.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ev
-
-# Run emulator
-echo no | android create avd --force -n test -t $ANDROID_TARGET --abi $ANDROID_ABI
-emulator -avd test -no-skin -no-audio -no-window &
-android-wait-for-emulator
-adb shell input keyevent 82 &
