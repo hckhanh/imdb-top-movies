76c01ed26faa9024d6b42cca17702a3f56105f20
hckhanh <hoangchaukhanh93@gmail.com>
2015-08-28 09:01:50 +0700

    Update testing method
    Remove Instrumentation Test
    Add Unit Test

--- travis-script/run-client-test.sh
+++ travis-script/run-client-test.sh
@@ -5,18 +5,11 @@ set -ev
 # Check whether 
 if [ -d "$ANDROID_DIR/$ANDROID_PROJECT_NAME" ]; then
 
-
 	cd $ANDROID_DIR/$ANDROID_PROJECT_NAME/
 	chmod ugo+x ./gradlew
 
-	# Build Android Apps
-	./gradlew build
-
-	# Run android emulator
-	$TRAVIS_BUILD_DIR/travis-script/run-emulator.sh
-
-	# Run connectedCheck (cC) of Android Apps
-	./gradlew cC
+	# Run unit tests for Android Apps
+	./gradlew test --continue
 
 	cd $TRAVIS_BUILD_DIR/
 
--- travis-script/run-emulator.sh
+++ travis-script/run-emulator.sh
@@ -3,7 +3,7 @@
 set -ev
 
 # Run emulator
-echo no | android create avd --force -n test -t android-22 --abi $ANDROID_ABI
+echo no | android create avd --force -n test -t $ANDROID_TARGET --abi $ANDROID_ABI
 emulator -avd test -no-skin -no-audio -no-window &
 android-wait-for-emulator
 adb shell input keyevent 82 &
