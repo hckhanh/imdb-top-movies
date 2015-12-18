7956b698d8a5ade24a7b492ee236d030d587c682
hckhanh <hoangchaukhanh93@gmail.com>
2015-12-19 00:45:48 +0700

    Fixed Travis scripts

--- travis-script/install-dependencies.sh
+++ travis-script/install-dependencies.sh
@@ -3,6 +3,6 @@
 set +e
 
 # Add dependencies here
-echo no | android create avd --force -n test -t $ANDROID_TARGET --abi $ANDROID_ABIS
+echo no | android create avd --force -n test -t $ANDROID_TARGET --abi $ANDROID_ABI
 emulator -avd test -no-skin -no-audio -no-window &
 android-wait-for-emulator
